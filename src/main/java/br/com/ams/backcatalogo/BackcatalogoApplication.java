77edff87f5c34016e1f0a4dee2973aa02c9108d0
AMS <amsraimar@gmail.com>
2022-08-31 17:57:10 -0300

    Inicial

---
+++ src/main/java/br/com/ams/backcatalogo/BackcatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ams.backcatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackcatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackcatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ams/backcatalogo/BackcatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ams.backcatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackcatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
